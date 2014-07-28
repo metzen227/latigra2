ef217baa721c9643401e01cfbce710e53dc5a80e
metzen227 <ccarus@yahoo.com>
2014-07-27 21:18:29 -0500

    updated names references to add additional items and blocks

--- src/main/java/com/metzen227/krystalcraft/reference/Names.java
+++ src/main/java/com/metzen227/krystalcraft/reference/Names.java
@@ -5,16 +5,55 @@ public class Names
     public static final class Blocks
     {
         public static final String ATTUNEMENT_TABLE = "attunementTable";
+        public static final String INFUSION_CHAMBER_BASE = "infusionChamberBase";
+        public static final String INFUSION_CHAMBER_CONTAINMENT = "infusionChamberContainment";
+        public static final String KRYSTAL_CONTAINMENT = "krystalContainment";
+        public static final String KRYSTAL_CHAMBER_BASE = "krystalChamberBase";
+        public static final String KRYSTAL_ACCUMULATOR_CONTROLLER = "krystalAccumulatorController";
+        public static final String KRYSTAL_ACCUMULATOR_ITEM_INTERFACE = "krystalAccumulatorItemInterface";
+        public static final String KRYSTAL_ACCUMULATOR_POWER_INTERFACE = "krystalAccumulatorPowerInterface";
+        public static final String KRYSTAL_ACCUMULATOR_CORE = "krystalAccumulatorCore";
+        public static final String KRYSTAL_ACCUMULATOR_FOCI_EMITTER = "krystalAccumulatorFociEmitter";
+        public static final String FOCI_PYLON = "fociPylon";
     }
 
     public static final class Items
     {
         public static final String ATTUNEMENT_ROD = "attunementRod";
+        public static final String KRYSTAL_SHARD_EMPTY = "krystalShardEmpty";
+        public static final String KRYSTAL_CLUSTER = "krystalCluster";
+        public static final String KRYSTAL_SETTING = "krystalSetting";
+        public static final String INFUSION_CORE = "infusionCore";
+        public static final String KRYSTAL_POUCH = "krystalPouch";
+        public static final String KRYSTAL_HOLDING = "krystalHolding";
+        public static final String KRYSTAL_TELEPORT = "krystalTeleport";
+        public static final String KRYSTAL_HEALING = "krystalHealing";
+        public static final String KRYSTAL_SHIELD = "krystalShield";
+        public static final String KRYSTAL_SPEED = "krystalSpeed";
+        public static final String KRYSTAL_SUSTAIN = "krystalSustain";
+        public static final String KRYSTAL_SHARPNESS = "krystalSharpness";
+        public static final String KRYSTAL_KNOCKBACK = "krystalKnockback";
+        public static final String KRYSTAL_LOOTING = "krystalLooting";
+        public static final String KRYSTAL_BURNING = "krystalBurning";
+        public static final String KRYSTAL_FREEZING = "krystalFreezing";
+        public static final String KRYSTAL_EXPERIENCE = "krystalExperience";
+        public static final String KRYSTAL_BLEEDING = "krystalBleeding";
+        public static final String KRYSTAL_NIGHTEYE = "krystalNightEye";
+        public static final String KRYSTAL_FEATHERFALL = "krystalFeatherFall";
+        public static final String KRYSTAL_LIGHT = "krystalLight";
+        public static final String KRYSTAL_REINFORCED = "krystalReinforced";
+        public static final String KRYSTAL_ASCENSION = "krystalAscension";
+        public static final String KRYSTAL_MENDING = "krystalMending";
+        public static final String KRYSTAL_FIRERESIST = "krystalFireResist";
+        
+
     }
 
     public static final class Tools
     {
-        public static final String ATTUNEMENT_ROD = "attunementRod";
+        //public static final String ATTUNEMENT_ROD = "attunementRod";
+        public static final String KRYSTAL_TOOLKIT = "krystalToolKit";
+        public static final String KRYSTAL_MANIPULATOR = "krystalManipulator";
     }
 
     public static final class Materials
